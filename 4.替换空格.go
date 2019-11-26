567e4618a9aa046c20b032a101227596bcc99cce
sangzhao2008@163.com <sangzhao2008@163.com>
2019-11-26 21:41:26 +0800

    4

---
+++ 4.替换空格.go
@@ -0,0 +1,11 @@
+package main
+
+//func main() {
+//	str := "We are happy."
+//	str1 := strings.Replace(str," ","",-1)
+//	fmt.Println(str1)
+//	if string(str[2])==" " {
+//		fmt.Println("空格")
+//		fmt.Println(str[2])
+//	}
+//}
